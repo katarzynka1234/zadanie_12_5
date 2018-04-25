72f294f5883851a37eea0c06736514e8eca6de34
Katarzyna Trześniewska <katarzynka.f@gmail.com>
2018-04-25 17:03:48 +0200

    Add script

--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+    var prefix = "https://cors-anywhere.herokuapp.com/";
+    var tweetLink = "https://twitter.com/intent/tweet?text=";
+    var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
 
-function getQuote() {
-    $.getJSON(quoteUrl, createTweet);
-};
-
-getQuote();
-$('.trigger').click(function(){
-    getQuote();
-});
-
-function createTweet (input) {
-    var data = input[0];
-
-    var quoteText = $(data.content).text().trim();
-    var quoteAutor = data.title;
-
-    if(!quoteAutor.length) {
-        quoteAutor = "Unknow author";
+    function getQuote() {
+        $.getJSON(prefix + quoteUrl, createTweet);
+        $.ajaxSetup({ cache: false });
     };
-};
 
-var tweetText = "Quote of the day: " + quoteText + "Author: " + quoteAutor;
-
-if (tweetText.length > 140) {
     getQuote();
-} else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    $('.quote').text(quoteText);
-    $('.author').text('Author: ' + quoteAutor);
-    $('.tweet').atrr('href', tweet);
-};
+    $('.trigger').click(function () {
+        getQuote();
+    });
 
+    function createTweet(input) {
+        var data = input[0];
 
+        var quoteText = $(data.content).text().trim();
+        var quoteAutor = data.title;
 
+        if (!quoteAutor.length) {
+            quoteAutor = "Unknow author";
+        };
+    };
+
+    var tweetText = "Quote of the day: " + quoteText + "Author: " + quoteAutor;
+
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text('Author: ' + quoteAutor);
+        $('.tweet').atrr('href', tweet);
+    };
 
 
 });
